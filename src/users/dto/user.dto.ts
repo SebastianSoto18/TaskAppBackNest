15aa69734bc1b5dc7e5d8d3076d431ad9ae59192
SebastianSoto18 <atlasjuanse12@gmail.com>
2023-03-12 17:48:22 -0500

    creando rutas user

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,5 +1,5 @@
 import { IsNotEmpty} from "class-validator";
-import { IsEnum, IsNumber, IsString } from "class-validator/types/decorator/decorators";
+import { IsEnum, IsNumber, IsOptional, IsString } from "class-validator/types/decorator/decorators";
 import { ROLES } from "src/constants/roles";
 
 export class UserDto{
@@ -20,6 +20,36 @@ export class UserDto{
     username: string;
 
     @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    age: number;
+
+    @IsEnum(ROLES)
+    @IsNotEmpty()
+    role: ROLES;
+}
+
+export class UserUpdateDto{
+    @IsOptional()
+    @IsString()
+    firstName: string;
+
+    @IsOptional()
+    @IsString()
+    lastName: string;
+
+    @IsOptional()
+    @IsString()
+    email: string;
+
+    @IsOptional()
+    @IsString()
+    username: string;
+
+    @IsOptional()
     @IsString()
     password: string;
 
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,11 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { UserDto, UserUpdateDto } from '../dto/user.dto';
 import { UsersEntity } from '../entities/users.entity';
 
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(UsersEntity) private readonly usersRepository: Repository<UsersEntity>) { 
         
+    }
+
+    public async CreateUser(body : UserDto): Promise<UsersEntity>{
+        try{
+            return await this.usersRepository.save(body);
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    public async FindUsers(body : UserDto): Promise<UsersEntity[]>{
+        try{
+             return await this.usersRepository.find();
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    public async FindUserById(id : string): Promise<UsersEntity>{
+        try{
+            return await this.usersRepository.createQueryBuilder('users').where({id}).getOne();
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    public async UpdateUser(id : string, body : UserUpdateDto): Promise<UpdateResult | undefined>{
+        try{
+            const user : UpdateResult = await this.usersRepository.update(id, body);
+            if(user.affected == 0){
+                return undefined;
+            }
+            return user;
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    public async DeleteUser(id : string): Promise<DeleteResult| undefined>{
+        try{
+            const user : DeleteResult = await this.usersRepository.delete(id);
+            if(user.affected == 0){
+                return undefined;
+            }
+            return user;
+        }catch(error){
+            console.log(error);
+        }
     }
 }
